04ce69e62c66cf5b47ecf55ca3fc3d5f952148ba
crumplecup <erik.w.rose@gmail.com>
2025-03-30 17:34:24 -0700

    refactor: `lib.rs` updated to reflect name and module changes.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ mod error;
 mod free;
 mod history;
 mod json;
+mod key_sets;
+mod keys;
 mod method;
 mod naics;
 mod parameter;
@@ -20,27 +22,40 @@ mod results;
 mod validate;
 mod value;
 
-pub use app::{App, ResultStatus};
+pub use app::{App, BTreeKeyMissing, RateLimit, ResultStatus};
 pub use config::{Options, ParameterKind};
 pub use data::{Data, FixedAssetData, GdpData, GdpDatum, MneDiData, NipaData};
 pub use dataset::{Dataset, DatasetDetails, Datasets};
+// investment invalid unused
+// check unused
 pub use error::{
-    AnnotationMissing, BTreeKeyMissing, BeaErr, BeaErrorKind, BoolInvalid, Check, Csv,
-    DatasetMissing, DeriveFromStr, EnvError, FromStrError, IntegerInvalid, InvestmentInvalid,
-    IoError, Jiff, JsonParseError, JsonParseErrorKind, KeyMissing, Nom, NotArray, NotFloat,
-    NotInteger, NotObject, NotParameterName, NotQuarter, OwnershipInvalid,
-    ParameterValueTableVariant, ParseFloat, ParseInt, ParseInteger, RateLimit, ReqwestError,
-    RowCodeMissing, SerdeJson, Set, UrlParseError, VariantMissing, YearInvalid,
+    BeaErr, BeaErrorKind, DatasetMissing, DeriveFromStr, FromStrError, IoError, Jiff,
+    JsonParseError, JsonParseErrorKind, KeyMissing, NotArray, NotObject,
+    ParameterValueTableVariant, ParseInt, ReqwestError, SerdeJson, Set, VariantMissing,
 };
 pub use free::{
     bea_data, file_size, from_csv, init, json_bool, json_str, map_to_bool, map_to_float,
-    map_to_int, map_to_string, to_csv, trace_init,
+    map_to_int, map_to_string, to_csv, trace_init, Csv, EnvError, NotFloat, NotInteger, ParseFloat,
+    ParseInteger, UrlParseError,
 };
 pub use history::{Chunk, Chunks, History};
 pub use json::Json;
+pub use key_sets::{
+    FixedAssets, GdpByIndustry, Ita, ItaData, ItaDatum, Mne, NiUnderlyingDetail, Nipa, NipaIterator,
+};
+pub use keys::{
+    date_by_period, parse_year, roman_numeral_quarter, AffiliateKind, AffiliateLevel,
+    AnnotatedInteger, Annotation, AnnotationMissing, AreaOrCountry, BoolInvalid, BoolOptions,
+    DirectionOfInvestment, Footnotes, Frequencies, Frequency, FrequencyOptions, Indicator, Integer,
+    IntegerInvalid, IntegerKind, IntegerOptions, InvestmentKind, ItaFrequencies, ItaFrequency,
+    Millions, MillionsOptions, NipaRange, NipaRangeIterator, NipaRanges, NipaTableName, Nom,
+    NotQuarter, OwnershipInvalid, OwnershipKind, OwnershipLevel, RowCode, RowCodeMissing,
+    SelectionKind, SelectionSet, State, StateKind, TableName, Year, YearInvalid, YearKind,
+    YearOptions, YearRange,
+};
 pub use method::Method;
 pub use naics::Naics;
-pub use parameter::{deserialize_bool, Parameter, ParameterName, Parameters};
+pub use parameter::{deserialize_bool, NotParameterName, Parameter, ParameterName, Parameters};
 pub use parameter_value::{
     Metadata, MneDoi, NipaFrequency, NipaShowMillions, NipaTable, NipaYear, ParameterFields,
     ParameterValueTable, ParameterValues,
@@ -50,13 +65,6 @@ pub use queue::{Event, Mode, Queue, Tracker};
 pub use request::{Request, RequestParameter, RequestParameters};
 pub use results::{BeaResponse, Results};
 pub use value::{
-    date_by_period, parse_year, roman_numeral_quarter, AffiliateKind, AffiliateLevel,
-    AnnotatedInteger, Annotation, ApiMetadata, AreaOrCountry, BoolOptions, DirectionOfInvestment,
-    FixedAssets, Footnotes, Frequencies, Frequency, FrequencyOptions, GdpByIndustry, Iip,
-    Indicator, InputOutput, Integer, IntegerKind, IntegerOptions, IntlServSta, IntlServTrade,
-    InvestmentKind, Ita, ItaData, ItaDatum, ItaFrequencies, ItaFrequency, Millions,
-    MillionsOptions, Mne, NiUnderlyingDetail, Nipa, NipaIterator, NipaRange, NipaRangeIterator,
-    NipaRanges, OwnershipKind, OwnershipLevel, Regional, RowCode, SelectionKind, SelectionSet,
-    State, StateKind, TableName, UnderlyingGdpByIndustry, ValueSet, ValueSets, Year, YearKind,
-    YearOptions, YearRange,
+    ApiMetadata, Iip, InputOutput, IntlServSta, IntlServTrade, Regional, UnderlyingGdpByIndustry,
+    ValueSet, ValueSets,
 };
