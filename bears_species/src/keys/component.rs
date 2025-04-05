51ac5e6077aa913f49d5dd7eb0d479c7a585a908
crumplecup <erik.w.rose@gmail.com>
2025-04-05 10:42:33 -0700

    feat: Component enum added for the IIP dataset.

---
+++ bears_species/src/keys/component.rs
@@ -0,0 +1,53 @@
+#[derive(
+    Debug,
+    Copy,
+    Clone,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Hash,
+    serde::Deserialize,
+    serde::Serialize,
+    strum::EnumIter,
+    derive_more::Display,
+    derive_more::FromStr,
+)]
+/// Represents different types of components used for position changes in financial accounts.
+pub enum Component {
+    /// Change in position
+    ChgPos,
+    /// Change in position attributable to changes in volume and valuation n.i.e.
+    ChgPosNie,
+    /// Change in position not attributable to financial-account transactions
+    ChgPosOth,
+    /// Change in position attributable to price changes
+    ChgPosPrice,
+    /// Change in position attributable to financial-account transactions
+    ChgPosTrans,
+    /// Change in position attributable to exchange-rate changes
+    ChgPosXRate,
+    /// Position
+    Pos,
+}
+
+impl Component {
+    /// Returns the description of the component as a static string.
+    pub fn description(&self) -> &'static str {
+        match self {
+            Component::ChgPos => "Change in position",
+            Component::ChgPosNie => {
+                "Change in position attributable to changes in volume and valuation n.i.e."
+            }
+            Component::ChgPosOth => {
+                "Change in position not attributable to financial-account transactions"
+            }
+            Component::ChgPosPrice => "Change in position attributable to price changes",
+            Component::ChgPosTrans => {
+                "Change in position attributable to financial-account transactions"
+            }
+            Component::ChgPosXRate => "Change in position attributable to exchange-rate changes",
+            Component::Pos => "Position",
+        }
+    }
+}
