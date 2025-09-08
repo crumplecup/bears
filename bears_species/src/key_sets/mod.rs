624f092ad80d6498cc2d8027a5ea45b49eb428c6
Erik Rose <erik.w.rose@gmail.com>
2025-09-08 10:46:04 -0700

    chore: Visibility updated for new types and some restructuring.

--- bears_species/src/key_sets/mod.rs
+++ bears_species/src/key_sets/mod.rs
@@ -12,14 +12,14 @@ mod regional;
 
 pub use fixed_assets::FixedAssets;
 pub use gdp_by_industry::{
-    GdpByIndustry, GdpData, GdpDatum, UnderlyingGdpByIndustry, UnderlyingGdpData,
+    GdpByIndustry, GdpCodes, GdpData, GdpDatum, UnderlyingGdpByIndustry, UnderlyingGdpData,
 };
 pub use iip::{Iip, IipData};
-pub use input_output::{InputOutput, InputOutputData};
+pub use input_output::{InputOutput, InputOutputData, IoCodes};
 pub use intl_serv_sta::IntlServSta;
 pub use intl_serv_trade::IntlServTrade;
 pub use ita::{Ita, ItaData, ItaDatum};
 pub use metadata::ApiMetadata;
 pub use mne::Mne;
-pub use nipa::{NiUnderlyingDetail, Nipa, NipaIterator};
+pub use nipa::{NiUnderlyingDetail, Nipa, NipaData, NipaIterator};
 pub use regional::Regional;
